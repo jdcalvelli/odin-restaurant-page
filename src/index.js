5b92dc3ef0906939e91c4a4097f7e4dc3a078de5
JD Calvelli <jdcalvelli@gmail.com>
2022-03-04 12:00:37 -0600

    resetting index.js to start on the homepage and clean up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { homePage } from "./homepage";
-import { menuPage } from "./menu";
-import { contactPage } from "./contact";
 
-//homePage.renderHomePage();
-//menuPage.renderMenuPage();
-contactPage.renderContactPage();
+homePage.renderHomePage();
+
+//could have put the tab switching functionality here, but it made
+//more sense to me to have it be a part of the nav.js module
+//bc its functionality specific to that module
